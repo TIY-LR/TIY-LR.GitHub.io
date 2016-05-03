bd55bfd5f53338d9a22e45c4e56df9e998f5f467
Daniel Pollock <daniellpollock@gmail.com>
2016-05-03 18:17:21 -0500

    Added files via upload

---
+++ studentcontroller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', []);
+
+    app.controller('studentCtrl', controller);
+
+    controller.$inject = ['$scope', '$http'];
+
+    function controller($scope, $http) {
+        activate();
+        function activate() {
+            $http.get("students.json").then(function(res) {
+                console.log(res.data);
+                $scope.students = res.data;
+            });
+
+        }
+    }
+})();
